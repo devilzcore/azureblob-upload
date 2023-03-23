8375f3742522cd8fdf0d9cc80335a601d0c7503d
devilzcore <aldosilva1@live.com>
2023-03-23 16:21:33 -0300

    chore: add ImageController

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace image_upload.Controllers
+{
+  public class ImageController
+  {
+
+  }
+}
