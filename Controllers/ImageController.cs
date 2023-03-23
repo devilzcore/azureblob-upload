82ea9889be0b9593c2d3f9abee8c186d9fab1174
devilzcore <aldosilva1@live.com>
2023-03-23 17:51:24 -0300

    feat: add image upload controller

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -1,12 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using image_upload.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace image_upload.Controllers
 {
-  public class ImageController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ImageController : ControllerBase
   {
+    private readonly AzureBlobStorageService _blobStorageService;
 
+    public ImageController(AzureBlobStorageService blobStorageService)
+    {
+      _blobStorageService = blobStorageService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> UploadFile(IFormFile file)
+    {
+      if (file == null || file.Length == 0)
+      {
+        return BadRequest("Please provide a file to upload.");
+      }
+
+      using (var stream = file.OpenReadStream())
+      {
+        string fileName = file.FileName;
+        string url = await _blobStorageService.UploadFileAsync(fileName, stream);
+        return Ok(url);
+      }
+    }
   }
 }
